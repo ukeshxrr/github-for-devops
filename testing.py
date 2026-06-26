a7f735ce79b97345a023132b4a6f16f7bd2db0da
Keshar Upadhyay <kesarupadhyaya@gmail.com>
2026-06-26 15:05:43 +0530

    added testing.py

--- testing.py
+++ testing.py
@@ -1,2 +1,3 @@
 print("hello im keshar how are you ?")
 print("i have added this to my git reoisitories ")
+print("this is the new changes that i did to understand the local and github changes criteria")
